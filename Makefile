4cc6159235242ef8cd2b0a81efba51ed92c96e32
Dario Hamidi <dario@gowriteco.de>
2014-05-26 23:17:10 +0200

    Actually test buffer

--- Makefile
+++ Makefile
@@ -12,5 +12,10 @@ buffer.o: util.o config.h script/compile buffer.h buffer.c
 buffer_test: buffer_test.o buffer.o util.o script/link
 	script/link $@ buffer_test.o buffer.o util.o
 
+
+test: script/test buffer_test
+	script/test buffer_test
+
 clean:
 	rm -vf ./buffer_test ./*.o
+	rm -vf ./*_actual
--- buffer_test.c
+++ buffer_test.c
@@ -1,5 +1,6 @@
 #include "buffer.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
    BUFFER buffer = Buffer.new(1024);
@@ -7,6 +8,7 @@ int main(int argc, char** argv) {
    size_t string_len = 0;
 
    Buffer.puts(buffer, "hello, world");
+   printf("%ld\n", (long int)Buffer.point(buffer));
    puts(Buffer.string(buffer, &string, &string_len));
 
    Buffer.backward(buffer, 5);
@@ -21,5 +23,18 @@ int main(int argc, char** argv) {
    Buffer.forward(buffer, 4);
    Buffer.puts(buffer," t");
    puts(Buffer.string(buffer, &string, &string_len));
+
+   Buffer.delete_to_beginning(buffer);
+   puts(Buffer.string(buffer, &string, &string_len));
+
+   Buffer.delete_to_end(buffer);
+   puts(Buffer.string(buffer, &string, &string_len));
+
+   Buffer.puts(buffer, "κάθαρσις");
+   puts(Buffer.string(buffer, &string, &string_len));
+
+   Buffer.destroy(&buffer);
+
+   free(string);
    return 0;
 }
