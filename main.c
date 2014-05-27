6a44fd2c63082df929468c5da695b02f31d66d24
Dario Hamidi <dario.hamidi@xing.com>
2014-05-27 23:14:19 +0200

    Small demo

--- main.c
+++ main.c
@@ -61,10 +61,92 @@ void teardown(void) {
    }
 }
 
+enum key {
+   KEY_CTRL_A = 0x1,
+   KEY_CTRL_B = 0x2,
+   KEY_CTRL_D = 0x4,
+   KEY_CTRL_E = 0x5,
+   KEY_CTRL_F = 0x6,
+   KEY_CTRL_K = 0xb,
+   KEY_CTRL_U = 0x15,
+   KEY_CTRL_P = 0x10,
+   KEY_CTRL_N = 0xe,
+   KEY_ENTER  = 0xa,
+};
+
+typedef int (*action_fn)(MENU);
+
+static int action_select_next(MENU menu) {
+   Menu.select_next(menu);
+   return 1;
+}
+
+static int action_select_prev(MENU menu) {
+   Menu.select_prev(menu);
+   return 1;
+}
+
+static int action_delete_char(MENU menu) {
+   Buffer.delete(Menu.buffer(menu));
+   Menu.match(menu);
+   return 1;
+}
+
+static int action_delete_to_beginning(MENU menu) {
+   Buffer.delete_to_beginning(Menu.buffer(menu));
+   Menu.match(menu);
+   return 1;
+}
+
+static int action_delete_to_end(MENU menu) {
+   Buffer.delete_to_end(Menu.buffer(menu));
+   Menu.match(menu);
+   return 1;
+}
+
+static int action_forward(MENU menu) {
+   Buffer.forward(Menu.buffer(menu), 1);
+   return 1;
+}
+
+static int action_backward(MENU menu) {
+   Buffer.backward(Menu.buffer(menu), 1);
+   return 1;
+}
+
+static int action_beginning_of_line(MENU menu) {
+   Buffer.backward(Menu.buffer(menu), 10000);
+   return 1;
+}
+
+static int action_end_of_line(MENU menu) {
+   Buffer.forward(Menu.buffer(menu), 10000);
+   return 1;
+}
+
+static int action_confirm(MENU menu) {
+   fprintf(stdout, "%s\n", Menu.selection(menu));
+   return 0;
+}
+
+static action_fn KEYMAP[256] = {
+   [KEY_CTRL_P] = action_select_prev,
+   [KEY_CTRL_N] = action_select_next,
+   [KEY_CTRL_A] = action_beginning_of_line,
+   [KEY_CTRL_E] = action_end_of_line,
+   [KEY_CTRL_F] = action_forward,
+   [KEY_CTRL_B] = action_backward,
+   [KEY_CTRL_U] = action_delete_to_beginning,
+   [KEY_CTRL_K] = action_delete_to_end,
+   [KEY_CTRL_D] = action_delete_char,
+   [KEY_ENTER]  = action_confirm,
+};
+
 int main(int argc, char** argv) {
    const char** item = NULL;
    char buf[4] = { 0 };
    MENU menu = Menu.new();
+   action_fn action = NULL;
 
    Menu.set_prompt(menu, ">>");
    Menu.set_height(menu, 3);
@@ -77,14 +159,24 @@ int main(int argc, char** argv) {
    Menu.display(menu, stdout);
 
    setup();
+
    while ( read(STDIN_FILENO, &buf, 4) != 0 ) {
 
       if (buf[0] >= 0x20) {
          Buffer.putc(Menu.buffer(menu), buf[0]);
-         Menu.display(menu, stdout);
+         Menu.match(menu);
+      } else {
+         action = KEYMAP[ (unsigned char)buf[0] ];
+
+         if (action && ! action(menu) ) {
+               goto exit;
+         }
       }
 
+      Menu.display(menu, stdout);
    }
+
+  exit:
    teardown();
 
    Menu.destroy(&menu);
